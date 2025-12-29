ed814507aae869b7ae7f82a125e8730f326c4951
esyeibe <branmalawi@gmail.com>
2025-12-30 01:41:24 +0700

    betulin masalah signup

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/about', [HomeController::class, 'about'])->name('about');
 //     return view('home');
 // });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
